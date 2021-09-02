444746de4c864e2341f9a7f40a396e26e44b439a
DivyaTummala <divyareddytummala@gmail.com>
2021-09-02 21:20:50 +0530

    Added a testdemo class

---
+++ src/test/java/Mavensampleproj/SampleDemo/Demo.java
@@ -0,0 +1,12 @@
+package Mavensampleproj.SampleDemo;
+
+import org.testng.annotations.Test;
+
+public class Demo {
+
+	@Test
+	public void Testdemo() {
+		System.out.println("test method");
+
+	}
+}
